71188f546871cab477076f0f375ec0bd49875c2d
krish-anu <krishnaanu200302@gmail.com>
2026-09-25 08:24:14 +0530

    fix: update default EC2 instance type from t3.micro to t3.large

--- infra/terraform/aws-ec2/variables.tf
+++ infra/terraform/aws-ec2/variables.tf
@@ -13,7 +13,7 @@ variable "name_prefix" {
 variable "instance_type" {
   description = "EC2 instance type."
   type        = string
-  default     = "t3.micro"
+  default     = "t3.large"
 }
 
 variable "vpc_id" {
